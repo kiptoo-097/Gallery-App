47a031839a4986a90a84bfc98eddd09ce457852d
kiptoo-097 <kiptoo097.com>
2020-11-15 20:15:39 +0300

    Created urlpatterns to direct home

--- photos/urls.py
+++ photos/urls.py
@@ -3,3 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from .views import SearchResultsListView,SearchLocationListView
+
+urlpatterns=[
+    path('',views.home,name='home'),
+]
