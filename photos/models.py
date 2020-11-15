a88abd0f0245e3cdfede02eba9eb90ea73e55f11
kiptoo-097 <kiptoo097.com>
2020-11-15 18:20:28 +0300

    Created class Images

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    name = models.CharField(max_length = 60)
+    image=models.ImageField(upload_to ='images/')
+
+
+    
