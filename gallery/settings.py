0f9606e7e2f92b0e6beb0f49fe9e2e1181dd364e
kiptoo-097 <kiptoo097.com>
2020-11-15 20:29:17 +0300

    Added bootsrap3

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos'
+    'photos',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
