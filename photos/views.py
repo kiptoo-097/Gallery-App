d02be117cf274db6c3ab516549328328ff44948d
kiptoo-097 <kiptoo097.com>
2020-11-17 21:40:46 +0300

    Modified view methods

--- photos/views.py
+++ photos/views.py
@@ -1,32 +1,30 @@
-from django.shortcuts import render
-from django.views.generic import ListView,TemplateView
-from .models import Images
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+from .models import Image, Category, Location
 
 # Create your views here.
+
+
 def home(request):
-    images= Images.objects.all()
-    return render(request, "index.html", {"images":images})
+    images= Image.objects.all()
+    return render(request, "home.html", {"images":images})
+
+def about(request):
+    return render(request,"about.html")
 
 
-class Search(ListView):
-    model = Images
-    context_object_name = 'images_list'
-    template_name = 'search.html'
-    
-    def get_queryset(self): 
-        query = self.request.GET.get('q')
-        if Images.objects.filter(Q(image_category=query)):
-            return Images.objects.filter(Q(image_category=query))
+def search_category(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_category(search_term)
+        message = f"{search_term}"
 
-class SearchLocation(ListView):
-    model = Images
-    context_object_name = 'images_list'
-    template_name = 'location.html'
-    
+        return render(request, 'search.html', {"message": message, "image": searched_images})
 
-    def get_queryset(self): 
-        query = self.request.GET.get('q')
-        if Images.objects.filter(Q(image_location=query)):
-            return Images.objects.filter(Q(image_location=query))
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
 
+def getLocation(request,location):
+    locations = Image.getLocation(location)
+    return render(request,'location.html',{'image':locations})
