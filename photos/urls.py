8b7a59ec611b8630177cb7844576079d83ff360b
kiptoo-097 <kiptoo097.com>
2020-11-17 21:41:33 +0300

    Modified url paths

--- photos/urls.py
+++ photos/urls.py
@@ -3,10 +3,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
 from . import views
-from .views import Search,SearchLocation
+from .views import search_category,home
+
+
 
 urlpatterns=[
     path('',views.home,name='home'),
-    path('search/', Search.as_view(), name='search_results'),
-    path('search/', SearchLocation, name='search_by_location'),
+    path('search/', views.search_category, name='search_category'),
+    path('about/', views.about, name='about'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
